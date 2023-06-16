6c8b3bda223563271e105826a62d396b2c8f455d
mickeysrishti <Mukeshpathak345@gmail.com>
2023-06-17 00:21:15 +0530

    GSAP JS Used

---
+++ script.js
@@ -0,0 +1,53 @@
+
+gsap.registerPlugin(ScrollTrigger);
+ 
+
+gsap.from("#page1", {
+    y: 40,
+    delay: 1.5,
+    duration: 1
+    
+})
+gsap.from(".logo", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".navleft", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".navright", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".heading", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".navleftbottom", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".navrightbottom", {
+    y: 45,
+    delay: 1.5,
+    duration: 1.2
+    
+})
+gsap.from(".bottomcenter", {
+    y: 20,
+    delay: 1.5,
+    duration: 1.2,
+    zIndex: 9999
+    
+})
